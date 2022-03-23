35346cf5099aed61af454e5e02197b158e2a9ae7
RadkoZKraka <rkkrowicki@gmail.com>
2022-03-23 21:46:44 +0100

    Dodanie treści zadań.

--- Create Phone Number/Program.cs
+++ Create Phone Number/Program.cs
@@ -5,6 +5,7 @@ namespace Create_Phone_Number
 {
     internal class Program
     {
+        //Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
         public static void Main(string[] args)
         {
             int[] num = new int[]{1,3,3,2,5,2,1,4,7,6};
--- Split Springs/Program.cs
+++ Split Springs/Program.cs
@@ -4,7 +4,7 @@ namespace Split_Springs
 {
     internal class Program
     {
-        // zrobione
+        //Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
         public static void Main(string[] args)
         {
             string input;
