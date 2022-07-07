ce04da9f16cb5e7981e3674b275eb5cbe48a6f6e
RadkoZKraka <rkkrowicki@gmail.com>
2022-07-07 20:37:58 +0200

    Kontynuacja.

--- Sort binary tree by levels/Program.cs
+++ Sort binary tree by levels/Program.cs
@@ -5,47 +5,29 @@ namespace Sort_binary_tree_by_levels
 {
     internal class Program
     {
+        private static Queue<Node> queue = new Queue<Node>();
         //https://www.codewars.com/kata/52bef5e3588c56132c0003bc/train/csharp
         public static List<int> TreeByLevels(Node node)
         {
-            if (node == null)
-            {
-                return null;
-            }
-            var nodeL = node.Left;
-            var nodeR = node.Right;
-            var listOfNodesValues = new List<int>();
-            var listOfNodes = new List<Node>();
-            var list = TreeLevel(node, listOfNodes);
-            foreach (var nodes in list)
-            {
-                listOfNodesValues.Add(nodes.Value);
-            }
-            return listOfNodesValues;
+            SortTree(node);
         }
 
-        public static List<Node> TreeLevel(Node node, List<Node> listOfNodes)
+        public static void SortTree(Node node)
         {
-            var nodeL = node.Left;
-            var nodeR = node.Right;
             if (node != null)
             {
-                if (nodeL != null)
+                if (node.Left != null)
                 {
-                    listOfNodes.Add(nodeL);
-                    TreeLevel(nodeL,listOfNodes);
+                    queue.Enqueue(node.Left);
                 }
-                if (nodeR != null) 
+                if (node.Right != null)
                 {
-                    listOfNodes.Add(nodeR);
-                    TreeLevel(nodeR,listOfNodes);
+                    queue.Enqueue(node.Right);
                 }
+                
             }
-
-            
-            // TreeLevel(nodeL.Left, nodeR.Right, listOfNodes);
-            return listOfNodes;
         }
+        
         public static void Main(string[] args)
         {
             Console.WriteLine(TreeByLevels(new Node(new Node(null, new Node(null, null, 4), 2), new Node(new Node(null, null, 5), new Node(null, null, 6), 3), 1)));
